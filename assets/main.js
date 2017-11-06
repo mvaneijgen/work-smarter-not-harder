8f55ccd5128f2223f35055f4db5ece900026852c
Mitchel van Eijgen <mvaneijgen@gmail.com>
2017-11-06 08:56:50 +0100

    add layout tweaks

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,47 @@
-const parallaxDownElements = document.querySelectorAll('.parallaxDown');
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
 
-window.addEventListener('scroll', function(e) {
-  console.log(window.scrollY);
-  parallexDownFun(window.scrollY)
-})
+const parallaxDownElements = document.querySelector('.parallaxDown');
+const parallaxDownElementTop = parallaxDownElements.getBoundingClientRect().top ;
+
+function offset(elt) {
+  var rect = elt.getBoundingClientRect();
+  var bodyElt = document.body;
+
+  return {
+    top: rect.top + bodyElt .scrollTop,
+    left: rect.left + bodyElt .scrollLeft
+  }
+}
+console.log(parallaxDownElements.top);
+console.log(parallaxDownElements.left);
+
+var myEfficientFn = debounce(function() {
+  parallexDownFun(window.scrollY);
+}, 10);
+
+window.addEventListener('scroll', myEfficientFn);
 
 function parallexDownFun(scrollpos) {
-  Array.from(parallaxDownElements.forEach(fun‌​ction(e) {
-    var parallaxDownElementTop = parallaxDownElement.getBoundingClientRect().top;
-    var calculaedPosTop = parallaxDownElementTop - scrollpos;
-    parallaxDownElement.style.transform = 'translateY(-'+calculaedPosTop+'px)';
-  });
+  var calculaedPosTop = parseInt((parallaxDownElementTop - scrollpos) / 5);
+  parallaxDownElements.style.transform = 'translateY(-'+calculaedPosTop+'px)';
+  console.log(calculaedPosTop);
 }
 
 // Replace emoji of form
